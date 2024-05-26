06aca9c10c0418c3605a6d6926b6f65b1e350d82
farvell <farvellsmurf@gmail.com>
2024-05-26 06:43:28 +0200

    Initialized project with Create Vite App + React + Typescript + Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main className="h-full flex justify-center items-center">
+      <h1 className="p-6 font-bold text-8xl text-gray-800">
+        ProjectH
+      </h1>
+    </main>
+  )
+}
+
+export default App;
